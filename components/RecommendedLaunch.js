ce28b86a59bd7666bf8484e50c19530e4222df1a
havish10 <netlahavish@gmail.com>
2018-10-21 10:55:38 -0400

    edfr

--- components/RecommendedLaunch.js
+++ components/RecommendedLaunch.js
@@ -1,10 +1,34 @@
 import MDSpinner from 'react-md-spinner'
 
+function calculateDistance(x1, y1, x2, y2) {
+  return y2 - y1 + (x2 - x1)
+}
+
 export default ({ position, launches }) =>
   position ? (
     <div style={{ border: '1px solid', padding: '3px 5px' }}>
       <h2>Recommended Launch</h2>
-      {JSON.stringify(shitLog(launches.sort((x, y) => )))}
+      {JSON.stringify(
+        shitLog(
+          launches.launches
+            .filter(x => x.location.pads[0])
+            .sort(
+              (x, y) =>
+                calculateDistance(
+                  x.location.pads[0].longitude,
+                  position.coords.lon,
+                  x.location.pads[0].latitude,
+                  position.coords.lat,
+                ) -
+                calculateDistance(
+                  y.location.pads[0].longitude,
+                  position.coords.lon,
+                  y.location.pads[0].latitude,
+                  position.coords.lat,
+                ),
+            ),
+        ),
+      )}
     </div>
   ) : (
     <>
