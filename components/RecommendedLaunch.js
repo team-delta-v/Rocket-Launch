28a502701aab6a77440d95a1716b0ed1d89a8013
havish10 <netlahavish@gmail.com>
2018-10-21 12:55:41 -0400

    asdasd

--- components/RecommendedLaunch.js
+++ components/RecommendedLaunch.js
@@ -5,6 +5,7 @@ import {
   ButtonGroup,
   Card,
   CardBody,
+  Container,
   Button,
   CardTitle,
   CardText,
@@ -39,51 +40,87 @@ function getClosest(position, launches) {
 }
 
 export default ({ position, launches }) => {
-  const xx = position && getClosest(position, launches).slice(0, 1)
+  const b = position && getClosest(position, launches).slice(0, 1)
   return position ? (
-    xx.map(x => (
-      <Card style={{ width: '400px' }}>
-        <CardBody>
-          <CardTitle>Recommended Launch</CardTitle>
-          <CardSubtitle>{x.name}</CardSubtitle>
-          <CardText>{x.location.pads[0].agencies[0].name}</CardText>
-        </CardBody>
-        <CardImg top width="50%" src={x.rocket.imageURL} alt="Rocket" />
-        <CardBody>
-          <CardTitle>Where to view</CardTitle>
-          <CardText>
-            <Button
-              color="info"
-              target="_blank"
-              href={x.location.pads[0] ? x.location.pads[0].mapURL : ''}
-            >
-              Open Map
-            </Button>
-            <ButtonGroup>
-              <Button
-                color="info"
-                target="_blank"
-                href={`https://www.google.com/maps/search/Hotels/@${
+    b.map(x => (
+      <Container>
+        <Row>
+          <Col xs="6">
+            <Card style={{ width: '400px' }}>
+              <CardBody>
+                <CardTitle>Recommended Launch</CardTitle>
+                <CardSubtitle>{x.name}</CardSubtitle>
+                <CardText>{x.location.pads[0].agencies[0].name}</CardText>
+                <img
+                  style={{
+                    maxWidth: '100%',
+                    clipPath: 'inset(0px 50px 0px 50px)',
+                    transform: 'scale(1.45)',
+                  }}
+                  alt="weather"
+                  src={`https://renderer.yellowiki.xyz/screenshot?url=https://darksky.net/details/${
+                    x.location.pads[0].latitude
+                  },${x.location.pads[0].longitude}/${moment(x.isostart).format(
+                    'YYYY-MM-DD',
+                  )}/us12/en&selector=.dayExtras`}
+                />
+              </CardBody>
+              <CardImg top width="50%" src={x.rocket.imageURL} alt="Rocket" />
+              <CardBody>
+                <CardTitle>Where to view</CardTitle>
+                <CardText>
+                  <Button
+                    color="info"
+                    target="_blank"
+                    href={x.location.pads[0] ? x.location.pads[0].mapURL : ''}
+                  >
+                    Open Map
+                  </Button>
+                  <div style={{ height: '10px' }} />
+                  <ButtonGroup style={{ textAlign: 'center' }}>
+                    <Button
+                      color="primary"
+                      target="_blank"
+                      href={`https://www.google.com/maps/search/Hotels/@${
+                        x.location.pads[0].latitude
+                      },${x.location.pads[0].longitude},12.75z`}
+                    >
+                      Find nearby hotels
+                    </Button>
+                    <Button
+                      color="primary"
+                      target="_blank"
+                      href={`https://www.google.com/maps/search/attractions/@${
+                        x.location.pads[0].latitude
+                      },${x.location.pads[0].longitude},12.75z`}
+                    >
+                      Find nearby attractions
+                    </Button>
+                  </ButtonGroup>
+                  <CardText>
+                    Launches on {moment(x.isostart).calendar()}
+                  </CardText>
+                </CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <iframe
+                id="map-embed-iframe"
+                frameBorder="0"
+                height="500px"
+                width="100%"
+                src={`https://maps.darksky.net/@wind_speed,${
                   x.location.pads[0].latitude
-                },${x.location.pads[0].longitude},12.75z`}
-              >
-                Find nearby hotels
-              </Button>
-              <Button
-                color="info"
-                target="_blank"
-                href={`https://www.google.com/maps/search/attractions/@${
-                  x.location.pads[0].latitude
-                },${x.location.pads[0].longitude},12.75z`}
-              >
-                Find nearby attractions
-              </Button>{' '}
-            </ButtonGroup>
-          </CardText>
-
-          <CardText>Launches on {moment(x.isostart).calendar()}</CardText>
-        </CardBody>
-      </Card>
+                },${
+                  x.location.pads[0].longitude
+                },5?domain=&quot;+encodeURIComponent(window.location.href)+&quot;&auth=1540140785_b7ab7cf7f90f02bd0fe0259f7a4f027d&embed=true&amp;timeControl=false&amp;fieldControl=false&amp;defaultField=wind_speed&amp;defaultUnits=_mph`}
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     ))
   ) : (
     <>
